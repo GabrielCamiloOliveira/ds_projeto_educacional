49028bf229db91af337b07e436b022888c31a140
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2023-11-20 14:04:24 -0300

    Atualizações do front
    - Menu de seleção de dificuldade
    - Aprimoramento de animações

--- front-end/src/app/menu-dificuldade/menu-dificuldade.component.ts
+++ front-end/src/app/menu-dificuldade/menu-dificuldade.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-dificuldade',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-dificuldade.component.scss']
 })
 export class MenuDificuldadeComponent {
+
+  textoIniciante = 'Iniciante';
+  descIniciante1 = '+'
+  descIniciante2 = '-'
+  descIniciante3 = 'x'
+
+  textoModerado = 'Moderado';
+  descModerado1 = '()'
+  descModerado2 = '÷'
+
+  textoExperiente = 'Experiente';
+  descExperiente1 = '[]'
+  descExperiente2 = 'x²'
+
+  textoMestre = 'Mestre';
+  descMestre1 = '{}'
+  descMestre2 = '√'
+
+  constructor(private router: Router) {}
+  
+  retornarHome(): void {
+    this.router.navigate(['/']); //
+  }
+
+  handleButtonClick(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
 
 }
