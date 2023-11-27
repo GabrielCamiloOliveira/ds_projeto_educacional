e1a66debea23e5f056dac1e203f03194e9d72cb3
Murilo <mufreua10@gmail.com>
2023-11-26 23:06:44 -0300

    Iniciando a implementação da autenticação do estudante

--- backend/src/main/java/student/repository/StudentRepository.java
+++ backend/src/main/java/student/repository/StudentRepository.java
@@ -2,5 +2,9 @@ package student.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import student.entity.Student;
+import java.util.Optional;
 
-public interface StudentRepository extends JpaRepository<Student, Long> { }
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByUsername(String username);
+}
